ea5a6a4a3f85ce57a682c5fd258fd0edf39e6295
masakiyo okuhama <e165725@ie.u-ryukyu.ac.jp>
2017-02-04 17:16:50 +0900

    できてるか心配なのでもういっかいやっとく

--- src/main/java/jp/ac/urryukyu/ie/e165725/Main.java
+++ src/main/java/jp/ac/urryukyu/ie/e165725/Main.java
@@ -4,7 +4,7 @@ package jp.ac.urryukyu.ie.e165725;
  * Created by okuhamamasakiyo on 2017/01/25.
  */
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String args[]) {//他のクラスから呼び出している
         Firstfile firstfile = new Firstfile();
         firstfile.fileRead("/Users/okuhamamasakiyo/IdeaProjects/report7/src/main/java/jp/ac/urryukyu/ie/e165725/list1.txt");
         Secondfile secondfile = new Secondfile();
